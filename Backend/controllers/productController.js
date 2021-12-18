60fb3abba72ba38f92266345eff6f7473f4428a7
diku <brajeshmishra825@gmail.com>
2021-12-18 12:57:26 +0530

    Get all products and create a product route created

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,3 +1,20 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+
+// Create Product --Admin
+exports.createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Get all Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
