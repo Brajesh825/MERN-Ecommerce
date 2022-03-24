dc5b93d8724009cd0910098612dbaaeade95b0a5
brajeshmishra825 <brajeshmishra825@gmail.com>
2022-03-24 23:36:36 +0530

    mern ecommerce

--- Backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URI).then((data) => {
-    console.log(`Mongodb connected with server : ${data.connection.host}`);
-  });
-};
-
-module.exports = connectDB;
